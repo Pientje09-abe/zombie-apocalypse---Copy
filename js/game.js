4d7a2a33961a1850a6e48a399c2c3ef335034f35
Pientje09-abe <abesmits247@gmail.com>
2026-06-30 10:25:01 +0200

    randomized button and added a new button to the game interface. Updated the game logic to handle new button functionality.

--- js/game.js
+++ js/game.js
@@ -1,3 +1,20 @@
-document.addEventListener("DOMContentLoaded", function () {
-    
+const timingButton = document.getElementById("timing-button");
+
+setInterval(() => {
+    var x = Math.floor(Math.random() * 9) + 1;
+
+    if (x >= 9) {
+        timingButton.classList.remove("timing-button-red");
+        timingButton.classList.add("timing-button-green");
+
+        setTimeout(() => {
+            timingButton.classList.remove("timing-button-green");
+            timingButton.classList.add("timing-button-red");
+        }, 750);
+    }
+}, 250);
+document.getElementById("timing-button").addEventListener("click", () => {
+  if (timingButton.classList.contains("timing-button-green")) {
+    addScore(1) ;
+  }
 });
