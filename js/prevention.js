4ee6fbac4b8bff853c62438ab8806c8c729cebce
Rubenthepronoob <ruben.de.jong2009@outlook.com>
2026-06-30 14:41:27 +0200

    placed all the javascript in one file

--- js/prevention.js
+++
@@ -1,8 +0,0 @@
-// Prevent text selection and image dragging
-document.addEventListener('selectstart', function(e) {
-  e.preventDefault();
-});
-
-document.addEventListener('dragstart', function(e) {
-  e.preventDefault();
-});
